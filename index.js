7eb43d4980e688b37902310505cb712c57e5f446
joseph mwanzia <makamwanzia@gmail.com>
2024-11-01 23:55:48 +0300

    entry file for commands

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
 console.log("Hello wold!")
+const fsModule = require('fs');
+const yargs = require('yargs')
+
+const addList = {
+    command: 'add description <describe> amount <amnt>',
+    describe: 'adds an expense to the list',
+
+    builder: () => {
+        return yargs('describe', {
+            describe: 'Add a description to your expenses',
+            type: 'string',
+            demandOption: true
+        })
+    },
+
+    handler: async (argv) => {
+        fsModule.readFile('./db.json', 'utf8', async (err, data) => {
+            if (err) {
+                console.error('Invalid data!:', err)
+                return;
+            }
+
+            const response = await data.json();
+            console.log(response);
+            return response
+        })
+    }
+}

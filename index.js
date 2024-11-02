3cd1accf8df0d889fcd730e7d9820d916d7459cf
joseph mwanzia <makamwanzia@gmail.com>
2024-11-02 23:07:36 +0300

    add command,add description, and the decsription and amount builer

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 #!/usr/bin/env node
 
-console.log("Hello wold!")
-const fsModule = require('fs');
-const yargs = require('yargs')
-
-const addList = {
-    command: 'add description <describe> amount <amnt>',
-    describe: 'adds an expense to the list',
-
-    builder: () => {
-        return yargs('describe', {
-            describe: 'Add a description to your expenses',
+    // include command name 'add' and a description
+    .command('add', 'add a new expense', {
+        description: {
+            description: 'Enter your description',
             type: 'string',
             demandOption: true
-        })
+        },
+        amount: {
+            description: 'Enter your amount',
+            type: 'number',
+            demandOption: true
+        }
     },
-
-    handler: async (argv) => {
-        fsModule.readFile('./db.json', 'utf8', async (err, data) => {
-            if (err) {
-                console.error('Invalid data!:', err)
-                return;
-            }
-
-            const response = await data.json();
-            console.log(response);
-            return response
-        })
-    }
-}

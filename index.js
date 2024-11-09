06bcda8fcbcdf78ee3abb38b052420820666b0d8
Joseph Mwanzia <makamwanzia@gmail.com>
2024-11-09 21:15:50 +0300

    feat: Track summary for a specif year and month

--- index.js
+++ index.js
@@ -64,6 +64,51 @@ yargs(hideBin(process.argv))
         console.log(`Expense deleted successfully`);
     })
 
+    .command('summary', 'track summary for a specific month', {
+        month: {
+            type: 'number',
+            describe: 'enter the month you wish to view',
+            demandOption: true
+        }
+    }, async (argv) => {
+        const tasks = await readFromFiles();
+        
+        const filteredYear = argv._[1] // If the year is provided in the formatt '10 2022' as the last argument
+        ? tasks.filter((item) => {
+            const [day, month, year] = item.date.split("/"); // Split by "/"
+            const yearDate = parseInt(year, 10); // parse year to integer
+            return yearDate === argv._[1] // returns all matching items from filtered year in an []
+          })
+        : tasks.filter((item) => {
+            const [day, month, year] = item.date.split("/");
+            const yearDate = parseInt(year, 10);
+            return yearDate === new Date().getFullYear(); // returns an [] of all matching items filtered in current year
+        })
+        
+        const filteredMonths = filteredYear.filter(item => {
+            const [day, month, year] = item.date.split('/');
+            const mnth = parseInt(month, 10)
+            return mnth === argv.month // Returns all the months that match in an array
+        })
+
+
+        const totalMonthlyExpenses = filteredMonths.reduce((accumulator, task) => {
+            return accumulator += task.amount
+        }, 0) // accumulates all the amounts to a sum
+
+        const myYear = argv._[1] || new Date().getFullYear()
+
+        const yearAndMonth = Intl.DateTimeFormat(
+            'en-gb', { year: 'numeric', month: 'long' }
+        ).format(new Date(
+            myYear, 
+            argv.month - 1, 
+            new Date().getDate()
+        )); // returns the month name and year provided by the user
+
+        console.log(`Total expenses for ${yearAndMonth}: $${totalMonthlyExpenses}`);
+    })
+
     .demandCommand(1, 'You need at least one command before moving on')
     .help()
     .argv;
